9d92eca0bf46946ebf9f5cfb46f014a899803ac2
criiss93 <cristian.lineros@usach.cl>
2017-06-20 01:28:19 -0400

    arreglo junit

--- build/generated-sources/ap-source-output/project/entities/Products_.java
+++ build/generated-sources/ap-source-output/project/entities/Products_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-19T16:36:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-20T01:25:43")
 @StaticMetamodel(Products.class)
 public class Products_ { 
 
